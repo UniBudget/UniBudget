e5fe6bc5bb1c28b4eff500f8cabdb7c9aed13cd9
markeisa <www.6grahammarkeisa@gmail.com>
2020-11-28 00:44:14 -0500

    Updated code. (Profile and edit profile completed)

---
+++ app/src/main/java/com/example/unibudget/EditProfileActivity.java
@@ -0,0 +1,29 @@
+package com.example.unibudget;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfileActivity extends AppCompatActivity {
+    private Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BacktoProfile();
+            }
+    });
+}
+
+    private void BacktoProfile() {
+        Intent intent = new Intent(this,ProfileActivity.class);
+        startActivity(intent);
+    }
+    }
--- app/src/main/java/com/example/unibudget/ProfileActivity.java
+++ app/src/main/java/com/example/unibudget/ProfileActivity.java
@@ -3,18 +3,44 @@ package com.example.unibudget;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ProfileActivity extends AppCompatActivity {
+    private Button Edtbutton;
+    private Button logOtbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        CircleImageView circleImageView = (CircleImageView) findViewById(R.id.profile_image);
+        Edtbutton = (Button) findViewById(R.id.Edtbutton);
+        Edtbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EdtProfile();
+            }
+        });
+        logOtbutton = findViewById(R.id.logOtbutton);
+        logOtbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+                Toast.makeText(ProfileActivity.this,"Successfully Logout" ,Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
         //Initialize
@@ -42,4 +68,10 @@ public class ProfileActivity extends AppCompatActivity {
             });
 
 
-        }}
+        }
+
+    private void EdtProfile() {
+        Intent intent = new Intent(this,EditProfileActivity.class);
+        startActivity(intent);
+    }
+}
